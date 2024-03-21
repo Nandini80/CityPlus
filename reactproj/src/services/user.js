fb43118415e1f5a122913a1c286ff37fbccd8879
Aditya Ningule <adiningule2@gmail.com>
2024-03-21 08:40:47 +0530

    Fixed #19

--- reactproj/src/services/user.js
+++ reactproj/src/services/user.js
@@ -1,34 +1,38 @@
 //Like node controller
 import publicReq, { privateReq } from "./axios-config";
 
-const signupservice =(data)=>
-{
-    console.log("inside sinupservice")
-    return publicReq.post("/user/Signup",data);
-}; 
-
-const getUserService=()=>{
-    return privateReq.get("/user/CurrentUser");
+const signupservice = (data) => {
+  console.log("inside sinupservice");
+  return publicReq.post("http://localhost:2000/user/Signup", data);
 };
 
-const DistinctCatService =()=>
-{
-    return publicReq.get("/provider/distinct-services");
-}; 
-
-const fetchProviders=(data)=>{
-    return privateReq.post("/provider/fetchprovider",data);
+const getUserService = () => {
+  return privateReq.get("/user/CurrentUser");
 };
 
-const DistinctCityClient =()=>
-{
-    return publicReq.get("/client/distinct-city");
-}; 
+const DistinctCatService = () => {
+  return publicReq.get("/provider/distinct-services");
+};
 
-const fetchClient=(data)=>{
-    return privateReq.post("/client/fetchClient",data);
+const fetchProviders = (data) => {
+  return privateReq.post("/provider/fetchprovider", data);
+};
+
+const DistinctCityClient = () => {
+  return publicReq.get("/client/distinct-city");
+};
+
+const fetchClient = (data) => {
+  return privateReq.post("/client/fetchClient", data);
 };
 
 export {};
 
-export {getUserService,signupservice,DistinctCatService,fetchProviders,DistinctCityClient,fetchClient};
+export {
+  getUserService,
+  signupservice,
+  DistinctCatService,
+  fetchProviders,
+  DistinctCityClient,
+  fetchClient,
+};
